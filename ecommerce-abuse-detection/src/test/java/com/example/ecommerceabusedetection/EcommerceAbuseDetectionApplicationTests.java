254c8f200666abda348e9930a7f713ea9de9771a
seoyoung <seoyoung.project@gmail.com>
2026-08-09 19:50:36 -0700

    backend setting

---
+++ ecommerce-abuse-detection/src/test/java/com/example/ecommerceabusedetection/EcommerceAbuseDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceabusedetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAbuseDetectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
